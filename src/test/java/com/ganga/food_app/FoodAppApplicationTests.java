96c4d52113917f260ee7f223f825b5523f6119ef
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2024-06-27 07:22:50 +0530

    Project Setup + Configured Database Properties

---
+++ src/test/java/com/ganga/food_app/FoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganga.food_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
